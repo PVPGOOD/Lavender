4535803e57776f5fe1a146d29e3afa46e1b381d3
MrToi <mr.toing@outlook.com>
2024-04-28 00:51:28 +0800

    Initialize commits 梦开始的地方 始于2024/4/28

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lavender
+
+# 如何加快Maven 下载速度 
+参考文献：https://zhuanlan.zhihu.com/p/71998219
---
+++ src/main/java/io/justme/lavender/La.java
@@ -0,0 +1,12 @@
+package io.justme.lavender;
+
+import lombok.Getter;
+
+/**
+ * @author JustMe.
+ * @since 2024/4/28
+ **/
+@Getter
+public class La {
+    private final static La INSTANCE = new La();
+}
