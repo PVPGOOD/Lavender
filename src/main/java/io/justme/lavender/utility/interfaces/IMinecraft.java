ada76930844eb9eb7fbdc2651f531ba2e0369e2c
MrToi <mr.toing@outlook.com>
2024-04-30 00:34:36 +0800

    feat: 一个接口用于获取方块人实例

---
+++ src/main/java/io/justme/lavender/utility/interfaces/IMinecraft.java
@@ -0,0 +1,13 @@
+package io.justme.lavender.utility.interfaces;
+
+import net.minecraft.client.Minecraft;
+
+/**
+ * @author JustMe.
+ * @since 2024/4/29
+ **/
+public interface IMinecraft {
+
+    Minecraft mc = Minecraft.getMinecraft();
+
+}
