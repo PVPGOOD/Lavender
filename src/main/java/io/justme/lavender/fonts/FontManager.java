b924cd748ef5ab8800da94e44ccf72e6fe7cd5ba
MrToi <mr.toing@outlook.com>
2024-05-03 18:43:31 +0800

    style(fontManager): SFBold12 SFBold16

--- src/main/java/io/justme/lavender/fonts/FontManager.java
+++ src/main/java/io/justme/lavender/fonts/FontManager.java
@@ -9,11 +9,16 @@ import java.awt.*;
 @Getter
 @SuppressWarnings("all")
 public final class FontManager {
+
+    private final FontDrawer SFBold12;
+    private final FontDrawer SFBold14;
     private final FontDrawer SFBold18;
     private final FontDrawer SFBold22;
 
     public FontManager() {
         try {
+            SFBold12 = createByResource("SFBOLD.ttf",12f,true);
+            SFBold14 = createByResource("SFBOLD.ttf",14f,true);
             SFBold18 = createByResource("SFBOLD.ttf",18f,true);
             SFBold22 = createByResource("SFBOLD.ttf",22f,true);
         } catch (Throwable throwable) {
