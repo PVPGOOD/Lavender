ecd0e459b92021cf4bdc1b0e5ac16650083f0985
JustMe <mr.toing@outlook.com>
2024-08-09 14:55:43 +0800

    feat(NoSlowDown): Bypass Watchdog

--- src/main/java/io/justme/lavender/module/impl/movements/NoSlowDown.java
+++ src/main/java/io/justme/lavender/module/impl/movements/NoSlowDown.java
@@ -1,26 +1,28 @@
 package io.justme.lavender.module.impl.movements;
 
-import com.viaversion.viarewind.protocol.protocol1_8to1_9.Protocol1_8To1_9;
-import com.viaversion.viarewind.utils.PacketUtil;
-import com.viaversion.viaversion.api.Via;
-import com.viaversion.viaversion.api.protocol.packet.PacketWrapper;
-import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
-import com.viaversion.viaversion.api.type.Type;
-import de.florianmichael.vialoadingbase.ViaLoadingBase;
+import io.justme.lavender.La;
+import io.justme.lavender.events.game.EventTick;
+import io.justme.lavender.events.network.EventPacket;
 import io.justme.lavender.events.player.EventMotionUpdate;
 import io.justme.lavender.events.player.EventSlowDown;
 import io.justme.lavender.module.Category;
 import io.justme.lavender.module.Module;
 import io.justme.lavender.module.ModuleInfo;
+import io.justme.lavender.utility.network.PacketUtility;
+import io.justme.lavender.utility.player.PlayerUtility;
 import io.justme.lavender.value.impl.ModeValue;
+import lombok.Getter;
 import net.lenni0451.asmevents.event.EventTarget;
 import net.lenni0451.asmevents.event.enums.EnumEventType;
-import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemSword;
+import net.minecraft.item.*;
+import net.minecraft.network.play.client.C07PacketPlayerDigging;
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
+import net.minecraft.network.play.client.C09PacketHeldItemChange;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.EnumFacing;
 
+import java.util.ArrayList;
+
+@Getter
 @ModuleInfo(name = "NoSlowDown", description = "", category = Category.MOVEMENTS)
 public class NoSlowDown extends Module {
 
@@ -39,37 +41,136 @@ public class NoSlowDown extends Module {
 
     @EventTarget
     public void onSlowDown(EventSlowDown event) {
-        switch (mode.getValue()) {
-            case "Normal":
-                event.setCancelled(true);
-                break;
-            case "Watchdog":
-                if (mc.thePlayer.getHeldItem() != null && (mc.thePlayer.getHeldItem().getItem() instanceof ItemFood || mc.thePlayer.getHeldItem().getItem() instanceof ItemSword)) {
-                    event.setCancelled(true);
-                }
-                break;
+
+        switch (getMode().getValue()) {
+            case "Normal" -> event.setCancelled(true);
+            case "Watchdog" -> event.setCancelled(true);
         }
     }
 
+
     @EventTarget
-    public void onEvent(EventMotionUpdate event) {
-        switch (mode.getValue()) {
-            case "Watchdog":
-                if (event.getType() == EnumEventType.PRE) {
-                    if (mc.thePlayer.getHeldItem() == null) {
-                        break;
+    public void onTicks(EventTick eventTick) {
+
+
+        if (mc.thePlayer == null) {
+            return;
+        }
+
+        if (mc.thePlayer.onGround) {
+            offGroundTicks = 0;
+        } else {
+            offGroundTicks++;
+        }
+    }
+
+    private boolean send;
+    private int offGroundTicks;
+
+    @EventTarget
+    public void onMotionEvent(EventMotionUpdate event) {
+
+
+        if (mc.thePlayer == null) {
+            return;
+        }
+
+        switch (getMode().getValue()) {
+            case "Normal" -> {}
+            case "Watchdog" -> {
+
+                switch (event.getType()) {
+                    case PRE -> {
+
+
+                        if (mc.thePlayer.getHeldItem() != null) {
+                            if (mc.thePlayer.getHeldItem().getItem() instanceof ItemFood) {
+                                if (offGroundTicks == 2 && send) {
+                                    La.getINSTANCE().print("fake eat");
+
+                                    getPacketUtility().sendPacket(
+                                            new C08PacketPlayerBlockPlacement(new BlockPos(-1, -1, -1),
+                                                    255, mc.thePlayer.getHeldItem(),
+                                                    0, 0, 0));
+
+
+                                    send = false;
+                                } else if (mc.thePlayer.getHeldItem() != null && mc.thePlayer.isUsingItem()) {
+
+                                    if (mc.thePlayer.getHeldItem() != null) {
+                                        if (mc.thePlayer.getHeldItem().getItem() instanceof ItemFood) {
+                                            event.setY(event.getY() + 1E-14);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+
                     }
 
-                    if (mc.thePlayer.isUsingItem() && mc.thePlayer.getHeldItem().getItem() instanceof ItemFood && mc.thePlayer.ticksExisted % 3 == 0) {
-                        mc.thePlayer.sendQueue.addToSendQueue(new C08PacketPlayerBlockPlacement(new BlockPos(-1, -1, -1), EnumFacing.UP.getIndex(), null, 0.0F, 0.0F, 0.0F));
-                    }
-                    if ((mc.thePlayer.isBlocking() && mc.thePlayer.getHeldItem().getItem() instanceof ItemSword) && !mc.isSingleplayer() && !ViaLoadingBase.getInstance().getTargetVersion().isOlderThan(ProtocolVersion.v1_12) && !ViaLoadingBase.getInstance().getTargetVersion().isNewerThan(ProtocolVersion.v1_12_2)) {
-                        PacketWrapper useItem = PacketWrapper.create(29, null, Via.getManager().getConnectionManager().getConnections().iterator().next());
-                        useItem.write(Type.VAR_INT, 1);
-                        PacketUtil.sendToServer(useItem, Protocol1_8To1_9.class, true, true);
+                    case POST -> {
+
                     }
                 }
-                break;
+            }
         }
+    }
+
+    public final PacketUtility packetUtility = new PacketUtility();
+    @EventTarget
+    public void onPacket(EventPacket eventPacket) {
+
+        if (mc.thePlayer == null) {
+            return;
+        }
+
+        switch (getMode().getValue()) {
+            case "Normal" -> {
+            }
+            case "Watchdog" -> {
+                //跑吃
+                if (mc.thePlayer.getHeldItem() != null) {
+                    if (mc.thePlayer.getHeldItem().getItem() instanceof ItemFood) {
+                        if (eventPacket.getPacket() instanceof C08PacketPlayerBlockPlacement blockPlacement && !mc.thePlayer.isUsingItem()) {
+
+                            if (blockPlacement.getPlacedBlockDirection() == 255 && offGroundTicks < 2) {
+                                if (mc.thePlayer.onGround) {
+                                    mc.thePlayer.setJumping(false);
+                                    mc.thePlayer.jump();
+                                }
+                                send = true;
+                                eventPacket.setCancelled(true);
+                            }
+                        }
+                    }
+
+                    //拦截来自 KillAura 释放的爱
+                    if (PlayerUtility.isHoldingSword()) {
+                        if (eventPacket.getPacket() instanceof C07PacketPlayerDigging packet) {
+                            if (packet.getStatus() == C07PacketPlayerDigging.Action.RELEASE_USE_ITEM) {
+                                eventPacket.setCancelled(true);
+                                int slot = mc.thePlayer.inventory.currentItem;
+                                getPacketUtility().sendPacket(new C09PacketHeldItemChange(slot < 8 ? slot + 1 : 0));
+                                getPacketUtility().sendPacket(new C09PacketHeldItemChange(slot));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean isUsingFood() {
+
+        if (mc.thePlayer == null) {
+            return false;
+        }
+
+        if (mc.thePlayer.getItemInUse() == null) {
+            return false;
+        }
+        Item usingItem = mc.thePlayer.getItemInUse().getItem();
+        return mc.thePlayer.isUsingItem() && (usingItem instanceof ItemFood || usingItem instanceof ItemBucketMilk || usingItem instanceof ItemPotion);
     }
 }
