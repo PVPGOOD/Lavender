697407844b64fb626f6d3b75f7ec526f9b41d6c2
JustMe <mr.toing@outlook.com>
2025-05-11 15:04:07 +0800

    style(hitbox): 防呆设计

--- src/main/java/io/justme/lavender/module/impl/blatant/player/HitBox.java
+++ src/main/java/io/justme/lavender/module/impl/blatant/player/HitBox.java
@@ -1,8 +1,11 @@
 package io.justme.lavender.module.impl.blatant.player;
 
+import io.justme.lavender.La;
 import io.justme.lavender.module.Category;
 import io.justme.lavender.module.Module;
 import io.justme.lavender.module.ModuleInfo;
+import io.justme.lavender.ui.screens.notifacation.NotificationsEnum;
+import io.justme.lavender.value.impl.BoolValue;
 import io.justme.lavender.value.impl.NumberValue;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +21,24 @@ import lombok.Setter;
 public class HitBox extends Module {
 
     private final NumberValue numberValue = new NumberValue("size",0.1, 0.1, 1.0, 0.1);
+    private final BoolValue forceEnable = new BoolValue("ForceEnable", false);
 
+    @Override
+    public void onEnable(){
+        super.onEnable();
 
+        if (!forceEnable.getValue()) {
+
+            La.getINSTANCE().getNotificationsManager().push(
+                    "Hitbox",
+                    "由于该功能会造成慢狗 请勾上 forceEnable",
+                    NotificationsEnum.FAIL,3000);
+            setStatus(false);
+        }
+    }
+
+    @Override
+    public void onDisable(){
+        super.onDisable();
+    }
 }
