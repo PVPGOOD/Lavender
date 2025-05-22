719de15ee1507aa2eec01dbadba41c8c4c030338
JustMe <mr.toing@outlook.com>
2025-05-22 22:54:40 +0800

    feat(killAura): targetMark

--- src/main/java/io/justme/lavender/module/impl/blatant/fight/KillAura.java
+++ src/main/java/io/justme/lavender/module/impl/blatant/fight/KillAura.java
@@ -1,6 +1,5 @@
 package io.justme.lavender.module.impl.blatant.fight;
 
-import com.mojang.authlib.GameProfile;
 import com.viaversion.viarewind.protocol.protocol1_8to1_9.Protocol1_8To1_9;
 import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.protocol.packet.PacketWrapper;
@@ -12,6 +11,7 @@ import io.justme.lavender.events.game.EventTick;
 import io.justme.lavender.events.player.EventAttack;
 import io.justme.lavender.events.player.EventMotionUpdate;
 import io.justme.lavender.events.player.EventUpdate;
+import io.justme.lavender.events.render.Event3DRender;
 import io.justme.lavender.module.Category;
 import io.justme.lavender.module.Module;
 import io.justme.lavender.module.ModuleInfo;
@@ -32,7 +32,8 @@ import lombok.Setter;
 import net.lenni0451.asmevents.event.EventTarget;
 import net.lenni0451.asmevents.event.enums.EnumEventType;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.network.NetworkPlayerInfo;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityArmorStand;
@@ -42,15 +43,16 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.play.client.C02PacketUseEntity;
 import net.minecraft.network.play.client.C07PacketPlayerDigging;
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.WorldSettings;
+import org.lwjgl.opengl.GL11;
 import org.lwjglx.util.vector.Vector2f;
 
+import java.awt.*;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author JustMe.
@@ -85,17 +87,15 @@ public class KillAura extends Module implements IMinecraft {
 
     private final ModeValue
             switchMode = new ModeValue("Switch Mode", new String[]{"Single", "Switch"}, "Switch");
-    private final ModeValue
-            attackMode = new ModeValue("Attack Mode", new String[]{"Pre", "Post"}, "Pre");
 
     private final ModeValue
             rotationMode = new ModeValue("Rotation Mode", new String[]{"Normal", "Adaptive"}, "Normal");
 
     private final ModeValue autoblockModeValue = new ModeValue("AutoBlock Mode", new String[]{"Watchdog", "BlocksMC","Key","Visual"}, "Key");
+    private final ModeValue blockTimingModeValue = new ModeValue("AutoBlock Timing", new String[]{"Post", "Pre"}, "Pre");
 
     private final ModeValue
             blockMode = new ModeValue("Block Mode", new String[]{"Full","Half"}, "Half");
-    private final ModeValue blockTimingModeValue = new ModeValue("Block Timing", new String[]{"Post", "Pre","BeforeAttack","AfterAttack"}, "Pre");
 
     private final NumberValue
             cps = new NumberValue("CPS", 15.0, 1, 20.0, 1),
@@ -146,9 +146,8 @@ public class KillAura extends Module implements IMinecraft {
 
     }
 
-
     @EventTarget
-    public void onMotionUpdate(EventUpdate event) {
+    public void onUpdate(EventUpdate event) {
         double cpsMs = MathUtility.getRandomDouble(
                 1000 / getCps().getValue() + MathUtility.getRandomDouble(5,10),
                 1000 / getCps().getValue() - MathUtility.getRandomDouble(3,5));
@@ -320,10 +319,32 @@ public class KillAura extends Module implements IMinecraft {
         }
     }
 
+    private void executeBlockOrUnblock() {
+
+        switch (getBlockMode().getValue()) {
+            case "Full" -> {
+                if (shouldBlock()) {
+                    doBlock();
+                }
+            }
+            case "Half" -> {
+                if (shouldBlock()) {
+                    doBlock();
+                }
+
+                if (getBlockTicks() % 4 != 0 && isBlocking()) {
+                    doUnblock();
+                }
+            }
+        }
+
+    }
+
 
     private void doBlock(){
         setBlocking(true);
 
+        La.getINSTANCE().print("block");
         switch (getAutoblockModeValue().getValue()) {
             case "Watchdog" -> {
                 getPacketUtility().sendPacketFromLa(new C02PacketUseEntity(target, C02PacketUseEntity.Action.INTERACT));
@@ -360,7 +381,6 @@ public class KillAura extends Module implements IMinecraft {
                     if (La.getINSTANCE().getUserConnection() == null) {
                         La.getINSTANCE().print("UserConnection is null");
                     } else {
-                        La.getINSTANCE().print("unblock");
                         getPacketUtility().sendPacketFromLa(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN));
                     }
                 }
@@ -450,11 +470,152 @@ public class KillAura extends Module implements IMinecraft {
         return true;
     }
 
-    private List<String> getTablist() {
-        return mc.getNetHandler().getPlayerInfoMap().stream()
-                .map(NetworkPlayerInfo::getGameProfile)
-                .filter(profile -> profile.getId() != mc.thePlayer.getUniqueID())
-                .map(GameProfile::getName)
-                .collect(Collectors.toList());
+
+    @EventTarget
+    public void on3D(Event3DRender event3DRender) {
+        if (target == null || mc.theWorld == null) return;
+
+        for (Entity entity : mc.theWorld.getLoadedEntityList()) {
+            if (entity == target) {
+                int color = new Color(255,0,0, 32).getRGB();
+
+                drawBox(entity, color);
+            }
+        }
+    }
+
+
+    private void drawBox(Entity entity, int color) {
+        double x = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * mc.timer.renderPartialTicks - mc.getRenderManager().getRenderPosX();
+        double y = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * mc.timer.renderPartialTicks - mc.getRenderManager().getRenderPosY();
+        double z = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * mc.timer.renderPartialTicks - mc.getRenderManager().getRenderPosZ();
+
+        var box = entity.getEntityBoundingBox().offset(-entity.posX, -entity.posY, -entity.posZ).offset(x, y, z);
+
+        GL11.glPushMatrix();
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glDepthMask(true);
+
+        drawFilledBox(box, color);
+
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        GL11.glLineWidth(1.5F);
+        drawBoxOutline(box, color);
+
+        GL11.glDisable(GL11.GL_LINE_SMOOTH);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glPopMatrix();
+    }
+
+    private void drawBoxOutline(AxisAlignedBB box, int color) {
+        float red = (color >> 16 & 255) / 255.0F;
+        float green = (color >> 8 & 255) / 255.0F;
+        float blue = (color & 255) / 255.0F;
+        float alpha = (color >> 24 & 255) / 255.0F;
+
+        GL11.glColor4f(red, green, blue, alpha);
+
+        var tessellator = Tessellator.getInstance();
+        var worldRenderer = tessellator.getWorldRenderer();
+
+        worldRenderer.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION);
+
+        worldRenderer.pos(box.minX, box.minY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.minY, box.minZ).endVertex();
+
+        worldRenderer.pos(box.maxX, box.minY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.minY, box.maxZ).endVertex();
+
+        worldRenderer.pos(box.maxX, box.minY, box.maxZ).endVertex();
+        worldRenderer.pos(box.minX, box.minY, box.maxZ).endVertex();
+
+        worldRenderer.pos(box.minX, box.minY, box.maxZ).endVertex();
+        worldRenderer.pos(box.minX, box.minY, box.minZ).endVertex();
+
+        worldRenderer.pos(box.minX, box.maxY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.maxY, box.minZ).endVertex();
+
+        worldRenderer.pos(box.maxX, box.maxY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.maxY, box.maxZ).endVertex();
+
+        worldRenderer.pos(box.maxX, box.maxY, box.maxZ).endVertex();
+        worldRenderer.pos(box.minX, box.maxY, box.maxZ).endVertex();
+
+        worldRenderer.pos(box.minX, box.maxY, box.maxZ).endVertex();
+        worldRenderer.pos(box.minX, box.maxY, box.minZ).endVertex();
+
+        worldRenderer.pos(box.minX, box.minY, box.minZ).endVertex();
+        worldRenderer.pos(box.minX, box.maxY, box.minZ).endVertex();
+
+        worldRenderer.pos(box.maxX, box.minY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.maxY, box.minZ).endVertex();
+
+        worldRenderer.pos(box.maxX, box.minY, box.maxZ).endVertex();
+        worldRenderer.pos(box.maxX, box.maxY, box.maxZ).endVertex();
+
+        worldRenderer.pos(box.minX, box.minY, box.maxZ).endVertex();
+        worldRenderer.pos(box.minX, box.maxY, box.maxZ).endVertex();
+
+        tessellator.draw();
+    }
+
+    public void drawFilledBox(AxisAlignedBB box, int color) {
+        float red = (color >> 16 & 255) / 255.0F;
+        float green = (color >> 8 & 255) / 255.0F;
+        float blue = (color & 255) / 255.0F;
+        float alpha = (color >> 24 & 255) / 255.0F;
+
+        GL11.glPushMatrix();
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glDisable(GL11.GL_CULL_FACE);
+        GL11.glColor4f(red, green, blue, alpha);
+
+        Tessellator tessellator = Tessellator.getInstance();
+        var worldRenderer = tessellator.getWorldRenderer();
+
+        worldRenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
+
+        worldRenderer.pos(box.minX, box.minY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.minY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.minY, box.maxZ).endVertex();
+        worldRenderer.pos(box.minX, box.minY, box.maxZ).endVertex();
+
+        worldRenderer.pos(box.minX, box.maxY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.maxY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.maxY, box.maxZ).endVertex();
+        worldRenderer.pos(box.minX, box.maxY, box.maxZ).endVertex();
+
+        worldRenderer.pos(box.minX, box.minY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.minY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.maxY, box.minZ).endVertex();
+        worldRenderer.pos(box.minX, box.maxY, box.minZ).endVertex();
+
+        worldRenderer.pos(box.minX, box.minY, box.maxZ).endVertex();
+        worldRenderer.pos(box.maxX, box.minY, box.maxZ).endVertex();
+        worldRenderer.pos(box.maxX, box.maxY, box.maxZ).endVertex();
+        worldRenderer.pos(box.minX, box.maxY, box.maxZ).endVertex();
+
+        worldRenderer.pos(box.minX, box.minY, box.minZ).endVertex();
+        worldRenderer.pos(box.minX, box.minY, box.maxZ).endVertex();
+        worldRenderer.pos(box.minX, box.maxY, box.maxZ).endVertex();
+        worldRenderer.pos(box.minX, box.maxY, box.minZ).endVertex();
+
+        worldRenderer.pos(box.maxX, box.minY, box.minZ).endVertex();
+        worldRenderer.pos(box.maxX, box.minY, box.maxZ).endVertex();
+        worldRenderer.pos(box.maxX, box.maxY, box.maxZ).endVertex();
+        worldRenderer.pos(box.maxX, box.maxY, box.minZ).endVertex();
+
+        tessellator.draw();
+
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glPopMatrix();
     }
 }
